b54a461972c8be3b04e70c7079ea4644b0542948
PrzyluckiMaciej <mprzylucki02@gmail.com>
2024-07-03 21:10:15 +0200

    Unique player name

--- src/components/PlayerForm/index.jsx
+++ src/components/PlayerForm/index.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function PlayerForm(props) {
   const [player, setPlayer] = useState({ name: "" });
+  const [error, setError] = useState("");
 
   const handleChange = ({ currentTarget: input }) => {
     setPlayer({ ...player, [input.name]: input.value });
@@ -10,24 +11,29 @@ function PlayerForm(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleExit();
 
-    var arr = JSON.parse(
-        localStorage.getItem('playerList') || '[]'
-    );
-
-    arr.push(player);
-
-    localStorage.setItem("playerList", JSON.stringify(arr));
-    window.location.reload();
+    var arr = JSON.parse(localStorage.getItem("playerList") || "[]");
+    console.log(player);
+    console.log(arr);
+    console.log(arr.filter(i => i.name === player.name).length);
+    if (arr.filter(i => i.name === player.name).length>0) {
+      setError("Player already exists!");
+    } else {
+      handleExit();
+      arr.push(player);
+      localStorage.setItem("playerList", JSON.stringify(arr));
+      window.location.reload();
+    }
   };
 
   const handleExit = () => {
     var form = document.getElementById("form");
     form.classList.add(styles.popup_disappear);
     form.classList.remove(styles.popup_appear);
-    setTimeout(function(){props.toggle();}, 600);
-  }
+    setTimeout(function () {
+      props.toggle();
+    }, 600);
+  };
 
   return (
     <div id="form" className={`${styles.popup} ${styles.popup_appear}`}>
@@ -44,6 +50,7 @@ function PlayerForm(props) {
               className={styles.input}
             />
           </label>
+          {error && <div className={styles.error_msg}>{error}</div>}
           <button type="submit">Add</button>
           <button type="button" onClick={handleExit}>
             Close
--- src/components/Players/index.jsx
+++ src/components/Players/index.jsx
@@ -7,13 +7,13 @@ import { useEffect, useState } from "react";
 
 function Players(props) {
   const [showForm, setShowForm] = useState(false);
-  const [players, setData] = useState([]);
+  const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    const data  =localStorage.getItem("playerList");
-    if(data != null){
-        setData(JSON.parse(data));
-    }    
+    const data = localStorage.getItem("playerList");
+    if (data != null) {
+      setPlayers(JSON.parse(data));
+    }
   }, []);
 
   const toggleShowForm = () => {
@@ -22,18 +22,17 @@ function Players(props) {
 
   return (
     <div className={styles.playerList}>
-      {players != null ?
-        players.map((item) => (
-          <Player player={item} className={styles.playerContainer} />
-        )) : null}
+      {players != null
+        ? players.map((item) => (
+            <Player player={item} className={styles.playerContainer} />
+          ))
+        : null}
       <IconButton
         className={styles.addButton}
         icon={<Peoples />}
         onClick={toggleShowForm}
       ></IconButton>
-      {showForm ? (
-        <PlayerForm toggle={toggleShowForm}/>
-      ) : null}
+      {showForm ? <PlayerForm toggle={toggleShowForm} /> : null}
     </div>
   );
 }
